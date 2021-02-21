d7e71a913da4bda856f45576c0176ab0c9ca1153
Baboya Choch <BaboyaChoch@Gmail.com>
2021-02-20 23:25:36 -0600

    completed post_creation page and writing to realtime database

--- nGen/build/js/content_mngr.js
+++ nGen/build/js/content_mngr.js
@@ -1,11 +1,3 @@
-document.addEventListener("DOMContentLoaded", event =>{
-    const app = firebase.app();
-});
-
-export function test() {
-    console.log("this works!");
-};
-
 class search{
 
 }
@@ -16,4 +8,26 @@ class content{
 
 class readWrite{
     
+}
+
+function write(userId){
+    const database = firebase.database();
+
+    let title = document.getElementById('post_title').value;
+    let field = document.getElementById('select_field').value;
+    let content = document.getElementById('post_content').value;
+
+    database.ref('post/' + userId).set({
+        userId: userId,
+        title: title,
+        forum: field,
+        content: content,
+      });
+      
+    return true;
+}
+
+function sendData(userId){
+   write(userId);
+   setInterval(() => {location.href = 'index.html';},1000);
 }
--- nGen/build/js/index.js
+++ nGen/build/js/index.js
@@ -1,3 +1,8 @@
-import * as ctn from './content_mngr.js'
 
-ctn.test();
+
+
+
+
+
+
+
