1828b9cf4594b51e6533fa164691e4b2de00afb4
Brice <your@email.combricetruong67@gmail.com>
2021-03-10 12:50:28 -0600

    update branch

--- nGen/build/js/activity.js
+++ nGen/build/js/activity.js
@@ -1,52 +1,70 @@
-
-const countClicks = function(){
-
-  const storage = function( name, type ){
-      'use strict';
-      const engine = !type || type.toLowerCase() === 'local' ? localStorage : sessionStorage;
-
-      const set=function( data ){
-          engine.setItem(name, JSON.stringify(data));
-      };
-      const create=function(){
-        if( !exists()) set(arguments[0] || {});
-    };
-      const get=function(){
-          return exists(name)? JSON.parse( engine.getItem(name)) : false;
-      };
-      const remove=function(){
-          engine.removeItem(name);
-      };
-      const exists=function(){
-          return engine.getItem(name)==null ? false : true;
-      };
-
-      return Object.freeze({
-          set,
-          get,
-          exists,
-          create,
-          remove
-      });
-  }
-
-  let store = new storage('urls', 'sessionStorage');
-    store.create();
-
-  let data=store.get();
-      data[ location.href ]=data.hasOwnProperty(location.href) ? parseInt(data[location.href]) + 1 : 1;
-      data.total=data.hasOwnProperty('total') ? parseInt(data.total) + 1 : 1;
-
-      store.set(data);
-}
-document.addEventListener('DOMContentLoaded', countClicks);
-
-
-
 class recordRead{
+
+     countClicks() {
+
+        function storage(name, type) {
+            'use strict';
+
+            const engine = !type || type.toLowerCase() === 'local' ? localStorage : sessionStorage;
+    
+            function set(data) {
+                engine.setItem(name, JSON.stringify(data));
+            }
+
+            function create() {
+                if (!exists())
+                    set(arguments[0] || {});
+            }
+
+            function get() {
+                return exists(name) ? JSON.parse(engine.getItem(name)) : false;
+            }
+
+            function remove() {
+                engine.removeItem(name);
+            }
+
+            function exists() {
+                return engine.getItem(name) == null ? false : true;
+            }
+
+    
+            return Object.freeze({
+                set,
+                get,
+                exists,
+                create,
+                remove
+            });
+        }
+    
+        let store = new storage('urls', 'sessionStorage');
+        store.create();
+    
+        let data = store.get();
+        data[location.href] = data.hasOwnProperty(location.href) ? parseInt(data[location.href]) + 1 : 1;
+        data.total = data.hasOwnProperty('total') ? parseInt(data.total) + 1 : 1;
+    
+        store.set(data);
+    }
+
+    addEventListener(DOMContentLoaded, countClicks);
 
 }
 
 class movePosts{
+    constructor() {
+        this.database = firebase.database();
+      }
 
-};
+    orderPostByClicks(postId, field){
+        var order = this.database.ref(`${field}/posts/${postId}`).orderByChild('click');
+    }
+    
+    orderPost(){
+    
+    var topUserPostsRef = firebase.database().ref('user-posts/' + postId).orderByChild('click');
+    }
+
+}
+
