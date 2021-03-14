30f1bcbe4c107ab4f4eed6116c5a76a1480559cc
Ryan <rbourd4@lsu.edu>
2021-03-14 14:24:57 -0500

    working registration, login, and user databse

--- nGen/build/js/accounts.js
+++ nGen/build/js/accounts.js
@@ -1,14 +1,15 @@
 /* ===============================================================================================================
  * |  JavaScript file for all user account related functions                                                     |
  * |  Functions in this file include but are not limited to account registration, signing in, and authentication |
- * | References:                                                                                                 |
+ * |  References:                                                                                                 |
  * |  Google Firebase: https://firebase.google.com/docs/auth/web/firebaseui                                      |
  * |                                                                                                             |
  * ===============================================================================================================
  * | Accounts Branch designed by: Ryan Bourdais rbourd4                                                          |
  * |  Project led by Baboya Chock                                                                                |
- * |  Latest Update: March 11, 2021                                                                           |
- * =============================================================================================================== */
+ * |  Latest Update: March 11, 2021                                                                              |
+ * =============================================================================================================== 
+ */
 
 function signUpWithEmailPassword() {
   var email = document.getElementById("email").value;
@@ -17,7 +18,7 @@ function signUpWithEmailPassword() {
     .then((userCredential) => {
       // Signed in 
       var user = userCredential.user;
-      location.href = "index.html"
+      location.href = "06_new_account2.html"
       // ...
     })
     .catch((error) => {
@@ -26,6 +27,17 @@ function signUpWithEmailPassword() {
       // ..
     }
     );
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+    });
+  // [END auth_signin_password]
 }
 
 function googleLogin(){
@@ -47,11 +59,40 @@ function signInWithEmailPassword() {
     .then((userCredential) => {
       // Signed in
       var user = userCredential.user;
+      location.href = "index.html"
       // ...
     })
     .catch((error) => {
       var errorCode = error.code;
       var errorMessage = error.message;
     });
-  // [END auth_signin_password]
+}
+
+
+ 
+  
+  //initialize variables for saveUser()
+
+function saveUser(userName, userBbday, userId) {
+  var userId = firebase.auth().currentUser.uid;
+  var userName = document.getElementById("userName").value;
+  var userBday = document.getElementById("userBday").value;
+  //let newKey = this.database.ref().push().key;
+  firebase.database().ref(`users/${userId}/`).set({
+      userId: userId,
+      username: userName,
+      birthday: userBday,
+  });
+  location.href = "index.html"
+}
+function UserStateChecker(){
+
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+    // User is signed in.
+    console.log("there is a currently signed in user");
+    } else {
+      console.log("there is no signed in user");
+    }
+  });
 }
