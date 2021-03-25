de05689ae19269e9376e9403977863d2c1e09494
Baboya Choch <BaboyaChoch@Gmail.com>
2021-03-24 22:16:15 -0500

    save

--- nGen/build/js/index.js
+++ nGen/build/js/index.js
@@ -6,15 +6,18 @@ function UserStateChecker() {
 	firebase.auth().onAuthStateChanged(function (user) {
 		if (user) {
 			console.log("there is currently a  signed in user");
-			let btn = document.getElementById("account");
-			btn.action = "profile.html";
-			btn.children[0].innerHTML = "Profile";
-
+			
 			let logoutButton = document.getElementById("logout");
-
+		
 			logoutButton.style.visibility = "visible";
 			logoutButton.onclick = () => {console.log('hello')}
 			
+			if(location.pathname.substring(1) === 'profile.html'){
+				let btn = document.getElementById("account");
+				btn.action = "profile.html";
+				btn.children[0].innerHTML = "Profile";
+			}
+		
 		} else {
 			console.log("there is no signed in user");
 			let logoutButton = document.getElementById("logout");
@@ -22,6 +25,7 @@ function UserStateChecker() {
 		}
 	});
 }
+
 document.body.onload = function () {
 	UserStateChecker();
 };
